0f3747d1e582ea520dbc6e7442a4dda776f20475
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-07-30 17:28:17 -0500

    connection to DB established

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//config environmental variables
+dotenv.config();
+
+//connect to database
+mongoose.connect(process.env.MONGO_DB_URI).then(()=>{
+  console.log('Connected to MongoDB');  
+}).catch(()=>{
+    console.log('There was an Error connecting to DB..')
+});
 
 const app = express();
-
+const PORT = 5000;
 
 //Middleware
 app.use(express.json());
@@ -12,6 +24,7 @@ app.get(`/`,(req,res)=>{
     res.status(200).send(`<h1>Welcome To Home Page</h1>`);
 });
 
-app.listen(5000, ()=>{
-    console.log(`Server is listening on port 5000`);
+//Listen for connections
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}....`);
 });
