e26285301491d4fa25737a91bea130c367634995
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-08-07 16:45:47 -0500

    Initialized front_end, cleaned up back_end. Added a background to get started.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import FloatingShape from "./components/FloatingShape.jsx"
+
+function App() {
+  return (
+    <div
+      className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-sky-900 
+    flex items-center relative overflow-hidden">
+      <FloatingShape color="bg-blue-500" size="w-64 h-64" top="-5%" left="10%" delay={0}/>
+      <FloatingShape color="bg-sky-500" size="w-48 h-48" top="70%" left="80%" delay={5}/>
+      <FloatingShape color="bg-blue-300" size="w-32 h-32" top="40%" left="-10%" delay={2}/>
+    </div>
+  );
+}
+
+export default App;
