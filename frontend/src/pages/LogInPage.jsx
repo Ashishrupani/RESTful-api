7e7be925b88c92d57aa2e937671a388b8708cd17
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-08-08 11:57:45 -0500

    front_end progress...  signup page done. Added routes and a lot of styles

---
+++ frontend/src/pages/LogInPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import {motion} from "framer-motion";
+import {Mail, Lock, Loader} from "lucide-react";
+import {Link} from "react-router-dom";
+import Input from "../components/Input";
+
+const LogInPage = () => {
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+
+  return(<></>);
+}
+
+export default LogInPage
