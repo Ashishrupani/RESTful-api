17a87c688030497dcd5747870926bae80aeab3fd
Ashishrupani <143018180+Ashishrupani@users.noreply.github.com>
2025-09-03 17:34:20 -0500

    finished authStore. Added a date formatter.

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,15 +22,13 @@ const PORT = process.env.PORT || 5000;
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 
 //Routes
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 app.get(`/`,(req,res)=>{
-    console.log(`User has hit the server`);
-    res.status(200).send(`<h1>Welcome To Home Page</h1>`);
+    res.status(200).send({success: true, message : "Recieving..."});
 });
 
 
---
+++ frontend/src/utils/date.js
@@ -0,0 +1,16 @@
+//converts date from the database version to user friendly version
+export const formatDate = (dateString) => {
+	const date = new Date(dateString);
+	if (isNaN(date.getTime())) {
+		return "Invalid Date";
+	}
+
+	return date.toLocaleString("en-US", {
+		year: "numeric",
+		month: "short",
+		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: true,
+	});
+};
